2d9edd485e63e12d3af57cd7cdf92900bdca13da
karthick345 <136611365+karthick345@users.noreply.github.com>
2023-09-14 22:36:19 +0530

    Add files via upload

---
+++ chellange 2/chel2_1.py
@@ -0,0 +1,29 @@
+class BankAccount:
+    def __init__(self,account_holder_name,account_number,balance):
+        self.__account_holder_name=account_holder_name
+        self.__account_number=account_number
+        self.__account_balance=balance
+
+    def deposite(self,amount):
+        if(amount>0):
+            self.__account_balance+=amount
+            print("deposite amount Rs:{}, and Current balance after deposite Rs:{}".format(amount,self.__account_balance))
+        else:
+            print("Invalid deposite, Please try again")
+    def withdraw(self,amount):
+        if (amount>0 and self.__account_balance>=amount):
+            self.__account_balance-=amount
+            print("Withdraw Amount Rs:{}, and Current balance after withdraw Rs:{}".format(amount,self.__account_balance))
+        else:
+            print("Invalid Withdraw Ammount. Please try again")
+
+    def checkbalance(self):
+        print("Account Holder Name :{}, Your Account Number :{}, Your Current Balance Rs:{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+
+
+a=BankAccount(account_holder_name="karthick",account_number=834758934,balance=5000)
+a.withdraw(500)
+a.deposite(1000)
+a.checkbalance()
+
+    
